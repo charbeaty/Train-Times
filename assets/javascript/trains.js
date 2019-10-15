921ee0220e9f8f69f400e1fb7d4282823a297dcf
Charissa Beaty <char.beaty11@gmail.com>
2019-10-14 18:25:54 -0600

    got firebase to work

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -1,17 +1,20 @@
 // Initialize Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyB_wl0d4vgsOXMiIYzsFOiIryhnLoLmIZ8",
-    authDomain: "train-times-e99e3.firebaseapp.com",
-    databaseURL: "https://train-times-e99e3.firebaseio.com",
-    projectId: "train-times-e99e3",
-    storageBucket: "train-times-e99e3.appspot.com",
-    messagingSenderId: "170300299143",
-    appId: "1:170300299143:web:8b36db155e7652d859a522"
-  };
+   var config = {
+    apiKey: "AIzaSyA_ayk2t2bVv_LSwK62YayTqarnrKc7EKY",
+    authDomain: "mondaywednesdayrocks-7e396.firebaseapp.com",
+    databaseURL: "https://mondaywednesdayrocks-7e396.firebaseio.com",
+    projectId: "mondaywednesdayrocks-7e396",
+    storageBucket: "",
+    messagingSenderId: "558929409150",
+    appId: "1:558929409150:web:e836433c5e71dde736542f"
+    };
 
-  firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(config);
 
-  var database = firebase.database();
+
+    // Get a reference to the database service
+    var database = firebase.database();
+  
 
 // Submit Button
 $("#add-train-button").on('click', function(event) {
@@ -21,18 +24,18 @@ $("#add-train-button").on('click', function(event) {
     var trainName = $('#train-input').val().trim();
     var trainLocation = $('#location-input').val().trim();
     var trainTime = $('#time-input').val().trim();
-    var trainRate = $('#frequence-input').val();
+    var trainFrequency = $('#frequency-input').val();
     
     //Object to hold data
     var newTrain = {
         name: trainName,
         local: trainLocation,
         time: trainTime,
-        rate: trainRate
+        rate: trainFrequency
     };
 
     //Upload data to database
-    database.ref().set(newTrain);
+    database.ref().push(newTrain);
 
     //Clear text-boxes
     $('#train-input').val('');
@@ -48,21 +51,21 @@ database.ref().on('child_added', function(childSnapshot) {
     var trainName = childSnapshot.val().name;
     var trainLocation = childSnapshot.val().local;
     var trainTime = childSnapshot.val().time;
-    var trainRate = childSnapshot.val().rate;
+    var trainFrequency = childSnapshot.val().rate;
     
         //Format time
-        trainRate = parseInt(trainRate);
+        trainFrequency = parseInt(trainFrequency);
         trainTime = parseInt(trainTime);
     
-        var timeLeft = moment().diff(moment.unix(trainTime), 'minutes') % trainRate;
-        var minutesAway = trainRate - timeLeft;
+        var timeLeft = moment().diff(moment.unix(trainTime), 'minutes') % trainFrequency;
+        var minutesAway = trainFrequency - timeLeft;
         var nextTrain = moment().add(minutesAway, 'm').format('hh:mm A');
     
     //Create Table
     var trainRow = $('<tr>').append(
         $('<td>').text(trainName),
         $('<td>').text(trainLocation),
-        $('<td>').text(trainRate),
+        $('<td>').text(trainFrequency),
         $('<td>').text(nextTrain),
         $('<td>').text(minutesAway)
     );
